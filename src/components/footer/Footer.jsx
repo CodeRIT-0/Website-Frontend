67e1f549980530e737858040bcc9e5414315ccc9
Ishaan <ishaanshetty12@gmail.com>
2022-12-10 23:37:38 +0530

    footer and query

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import Queries from "../queries/Queries";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <Queries />
+      <div className="footer">
+        <div className="row">
+          <div className="col">
+            <img
+              src={require("./images/CodeRITFullWhite1.png")}
+              className="coderit-footer"
+            />
+          </div>
+          <div className="col">
+            <a href="#" className="footer-col">
+              Communities
+            </a>
+          </div>
+          <div className="col">
+            <a href="#" className="footer-col">
+              Useful links
+            </a>
+          </div>
+          <div className="col">
+            <ul>
+              <li>
+                <img src={require("./images/Instagram.png")} />
+              </li>
+              <li>
+                <img src={require("./images/icons8-linkedin-50 1.png")} />
+              </li>
+              <li>
+                <img src={require("./images/discord.png")} />
+              </li>
+              <li>
+                <img src={require("./images/Telegram.png")} />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
