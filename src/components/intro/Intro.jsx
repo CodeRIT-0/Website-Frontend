14f39e7ccdaac42bd337959ad51dfdace82dd7f4
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2022-12-11 17:53:54 +0530

    "Added the code and styling for Intro"

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./intro.css";
 
 const Intro = () => {
   return (
-    <div>Intro</div>
-  )
-}
+    <div id="intro">
+      <h1 id="intro-head">CodeRIT</h1>
+      <p id="intro-para">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. In animi porro
+        a at saepe cupiditate, sed harum omnis. Laudantium architecto dolore
+        similique impedit. Velit quia fuga laboriosam qui maiores reprehenderit
+        saepe sapiente, adipisci blanditiis est sed expedita vitae autem odit,
+        distinctio iusto. Architecto eveniet alias ratione, rem at mollitia
+        reprehenderit quo repellendus! Cumque maiores aperiam dignissimos
+        ratione distinctio cum maxime quod, velit tempore consectetur vero
+        voluptatum, alias aspernatur, a temporibus labore in neque voluptate
+        possimus rem esse? Placeat fugit quia omnis atque perferendis. Eos quis
+        debitis ipsa itaque accusantium nostrum harum odit veritatis modi dicta?
+        Aperiam sit laborum aut laudantium molestias vitae possimus. Voluptates
+        doloribus nam accusantium dolor qui, maxime quisquam tempora vitae,
+        consectetur cumque eum non nostrum alias excepturi ipsum repellendus
+        earum ut. Repellat saepe error assumenda delectus non natus rem minima
+        recusandae. Similique delectus cum maxime a cumque nihil culpa,
+        officiis, nostrum recusandae, nulla blanditiis. Aliquid voluptate nulla
+        ipsa illo, voluptatibus, odio vel, incidunt omnis dolorem a nihil
+        perspiciatis accusamus inventore vitae veritatis delectus totam magni
+        harum corporis doloribus deserunt ipsum rem esse voluptatem. Laboriosam
+        placeat quae ratione, pariatur quis odio sit voluptates aliquid
+        reprehenderit? Alias, qui natus. Architecto culpa nostrum odit, debitis
+        ipsam quis ducimus vitae dolor?
+      </p>
+    </div>
+  );
+};
 
-export default Intro
+export default Intro;
