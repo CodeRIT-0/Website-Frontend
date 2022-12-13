c517221e437e1d889868eb378ea17dfb855fc398
Prash2002 <1707prash@gmail.com>
2022-12-13 19:48:14 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ import Intro from "./components/intro/Intro";
 import Team from "./components/team/Team";
 import PastEvents from './components/events/PastEvents'
 import Queries from "./components/queries/Queries";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <div id="home">
+
       <Intro />
+      </div>
+      <div id="events">
+
       <EventsLanding />
+      </div>
+      <div id="pastev">
+
       <PastEvents/>
+      </div>
+      <div id="team">
+
       <Team />
+      </div>
+      <div id="faq">
+
       <FAQ />
+      </div>
+      <div id="queries">
+
       <Queries />
+      </div>
       <Footer />
     </div>
   );
