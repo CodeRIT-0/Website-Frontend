fab3760a66c7d54db8a2877c8ad5682a205a60c2
Prash2002 <1707prash@gmail.com>
2022-12-14 11:08:23 +0530

    favi-title

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -26,7 +26,7 @@ const Intro = () => {
           contests, workshop and information about competitions like ACM-ICPC,
           Google Code Jam, Google Summer of Code, and other online coding
           events. We conduct workshops on areas of computer science like web
-          development, resume building, git/GitHub etc. We conduct coding
+          development, resume building, Git/GitHub etc. We conduct coding
           competitions which are open to all branches with exciting prizes to
           the winners and best female coder - Coding Diva as well.
         </p>
